fa435dcd1f034a61a2f3d3b2a4feb80798c74f39
alumno <alumno@lab_quimica.uacj.mx>
2025-02-26 09:46:32 -0700

    [INICIALIZA] el proyecto de xcode

---
+++ Agenda Shuffle/Agenda Shuffle/Agenda_ShuffleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Agenda_ShuffleApp.swift
+//  Agenda Shuffle
+//
+//  Created by alumno on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Agenda_ShuffleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PantallaAgenda()
+        }
+    }
+}
