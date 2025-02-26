5b0fe6bea24b1f761cb5de46c6afd38550095b32
alumno <alumno@lab_quimica.uacj.mx>
2025-02-26 10:15:45 -0700

    [AGREGA] un componenete para la vista de contacto prevista

---
+++ Agenda Shuffle/Agenda Shuffle/contacto_prevista.swift
@@ -0,0 +1,22 @@
+//
+//  contacto_prevista.swift
+//  Agenda Shuffle
+//
+//  Created by alumno on 2/26/25.
+//
+
+import SwiftUI
+
+struct ContactoPrevista: View {
+    var body: some View {
+        HStack{
+            Image(systemName: "globe")
+            Text("Hello Darknes")
+            Text("My old friend")
+        }
+    }
+}
+
+#Preview {
+    ContactoPrevista()
+}
