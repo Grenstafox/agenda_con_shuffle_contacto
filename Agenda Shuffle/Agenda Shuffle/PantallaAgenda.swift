4758494168519f1e526254a18479f5c084e1767d
alumno <alumno@lab_quimica.uacj.mx>
2025-02-26 10:47:28 -0700

    [MODIFICA] la pantalla agenda para usar el modelo nuevo

--- Agenda Shuffle/Agenda Shuffle/PantallaAgenda.swift
+++ Agenda Shuffle/Agenda Shuffle/PantallaAgenda.swift
@@ -7,12 +7,27 @@
 
 import SwiftUI
 
+let contactos = [
+ContactoAgenda(nombre: "juan", telefono: "123456" ),
+ContactoAgenda(nombre: "juana", telefono: "123456" ),
+ContactoAgenda(nombre: "juancho", telefono: "123456" ),
+ContactoAgenda(nombre: "juanpa", telefono: "123456" ),
+ContactoAgenda(nombre: "joana", telefono: "123456" ),
+ContactoAgenda(nombre: "judini", telefono: "123456" ),
+ContactoAgenda(nombre: "juan", telefono: "123456" ),
+ContactoAgenda(nombre: "juan", telefono: "123456" ),
+ContactoAgenda(nombre: "juan", telefono: "123456" ),
+ContactoAgenda(nombre: "juan", telefono: "123456" ),
+ContactoAgenda(nombre: "juan", telefono: "123456" ),
+]
+
 struct PantallaAgenda: View {
     var body: some View {
         
         List{
             VStack {
-                ForEach(0...45, id: \.self){ _ in
+                ForEach(contactos){ contacto in
+                    Text("\(contacto.nombre)")
                     ContactoPrevista()
                 }
             }
@@ -20,6 +35,7 @@ struct PantallaAgenda: View {
         }
     }
 }
+
 
 #Preview {
     PantallaAgenda()
