c93cd811d6e0d4b751a21ac0750a8a333dfb5976
alumno <alumno@lab_quimica.uacj.mx>
2025-02-26 10:46:09 -0700

    [Agrega]un modelo de contacto para guardar informacion

---
+++ Agenda Shuffle/Agenda Shuffle/modelos/contacto_agenda.swift
@@ -0,0 +1,28 @@
+//
+//  contacto_agenda.swift
+//  Agenda Shuffle
+//
+//  Created by alumno on 2/26/25.
+//
+
+import SwiftUI
+
+struct ContactoAgenda: Identifiable {
+    var nombre: String
+    var telefono: String
+    
+    var id: String { nommbre }
+}
+
+/*
+var nombre = "Pepito Veraz"
+
+var _nombre: String
+
+var nombre: set{
+    let nombre_validado = filtrar_palabras(Value)
+    _nombre = nombre_validado
+} get {
+    return _nombre.split(" ")[0]
+}
+*/
